7210a43c8c753b6c6caeab88fc480f948f27e86a
EdhsonOb <edhson.obed85@gmail.com>
2024-06-24 14:03:44 -0600

    version final

--- server.js
+++ server.js
@@ -263,7 +263,7 @@ app.post('/enviar-formulario', async (req, res) => {
             try {
                 // Enviar el primer correo
                 await transporter.sendMail({
-                    from: '"BCL Management" <' + process.env.EMAIL_FROM + '>',
+                    from: '"BCL Management" <' + process.env.EMAIL_USER + '>',
                     to: correoAplicant,
                     subject: 'Detalles de tu solicitud de formulario',
                     html: htmlEmailContentCancel
@@ -271,7 +271,7 @@ app.post('/enviar-formulario', async (req, res) => {
 
                 // Enviar el segundo correo
                 await transporter.sendMail({
-                    from: '"BCL Management" <' + process.env.EMAIL_FROM + '>',
+                    from: '"BCL Management" <' + process.env.EMAIL_USER + '>',
                     to: formData.correo,
                     subject: 'Autorización de Solped Requerida',
                     html: htmlEmailContent,
